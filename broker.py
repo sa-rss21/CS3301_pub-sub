e56ce73afc52fa31a68e9f69e14eb3955324c979
sa-rss21 <rss21@st-andrews.ac.uk>
2023-11-09 09:19:14 +0000

    R1 and R2 done!

--- broker.py
+++ broker.py
@@ -3,8 +3,8 @@ import threading
 import time
 import xmlrpc.client
 import xmlrpc.server
-import cProfile
-
+import sys
+import json
 
 class MessageQueueManager:
     def __init__(self):
@@ -41,6 +41,9 @@ class MessageQueueManager:
 
 
 class MessageBroker:
+    """
+    Pub-sub Message Broker that uses XML middleware to publish and retreive messages from the MessageQueueManager
+    """
     def __init__(self, url):
         self.subscribers = {}
         self.message_queue = MessageQueueManager()
@@ -52,34 +55,80 @@ class MessageBroker:
         broker_thread.start()
 
     def start_listening(self):
+        """
+        Starts the XML server to receive requests from clients, either publishers or subscribers
+        :return: None
+        """
         server = xmlrpc.server.SimpleXMLRPCServer((self.host, self.port), allow_none=True)
         server.register_function(self.subscribe, "subscribe")
+        server.register_function(self.unsubscribe, "unsubscribe")
         server.register_function(self.publish, "publish")
         server.register_function(self.get_messages, "get_messages")
         server.serve_forever()
 
-    def subscribe(self, topic, id):
+    def subscribe(self, topic, subscriber_id):
+        """
+        Adds a subscriber to the MessageBroker's subscriber dictionary on the specified channel
+        :param topic: channel to subscribe to
+        :param subscriber_id: ID of the subscriber
+        :return: None
+        """
         if topic not in self.subscribers:
             self.subscribers[topic] = []
         if not self.message_queue.get_queue(topic):
             self.message_queue.create_queue(topic)
-        self.subscribers[topic].append(id)
+        self.subscribers[topic].append(subscriber_id)
 
-
-    def unsubscribe(self, topic, id):
-        if topic in self.subscribers and id in self.subscribers[topic]:
-            self.subscribers[topic].remove(id)
+    def unsubscribe(self, topic, subscriber_id):
+        """
+        Removes the subscriber ID from the specified channel
+        :param topic: channel to unsubscribe from
+        :param subscriber_id: ID of the subscriber
+        :return: None
+        """
+        if topic in self.subscribers and subscriber_id in self.subscribers[topic]:
+            self.subscribers[topic].remove(subscriber_id)
             if not self.subscribers[topic]:
                 del self.subscribers[topic]
 
-    def publish(self, topic, id, message):
+    def publish(self, message):
+        """
+        Publishes message to the message queue
+        :param message: message-package to unpack and send to message queue manager
+        :return: None
+        """
+        content, publisher_id, topic = message["content"], message["id"], message["topic"]
         if not self.subscribers.get(topic):
             self.subscribers[topic] = []
-        self.message_queue.publish_message(topic, id, message)
+        self.message_queue.publish_message(topic, publisher_id, content)
 
-    def get_messages(self, topic):
-        return list(self.message_queue.get_messages(topic))
+    def get_messages(self, topic, subscriber_id):
+        """
+        Retreives messages from the specified channel
+        :param topic: channel to fetch messages from
+        :param subscriber_id: ID of the subscriber requesting the message
+        :return: None
+        """
+
+        # checks that subscriber has access to the data
+        if subscriber_id in self.subscribers[topic]:
+            return list(self.message_queue.get_messages(topic))
+        else:
+            print("Subscriber attempting to retreive messages that it is not subscribed to")
+            return None
 
     def has_subscribers(self, topic):
         return len(self.subscribers.get(topic, [])) != 0
 
+
+if __name__ == "__main__":
+
+    num_args = len(sys.argv) - 1
+    if num_args == 1:
+        try:
+            # Use the dictionary in your code
+            broker = MessageBroker(sys.argv[1])
+        except Exception as e:
+            print("Error starting broker:", e)
+    else:
+        print("Usage: python script.py 'url'")
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import cProfile
 import string
 import time
-import xmlrpc.server
 import random
 
 from broker import MessageBroker
@@ -26,30 +24,25 @@ if __name__ == "__main__":
     start = time.time()
     # Start the broker server in a separate thread
 
-    num_clients = 10
+    num_clients = 1000
     broker_url = "http://localhost:8000"  # URL of the message broker server
 
     topic = "T1"
     topic2 = "T2"
     broker = MessageBroker(broker_url)
-    subscriber = Subscriber(1, broker_url)
+    subscriber = Subscriber(1, broker_url, poll_interval=2)
     subscriber.subscribe(topic)
-
-    subscriber2 = Subscriber(2, broker_url)
-    subscriber2.subscribe(topic2)
-
+    subscriber.subscribe(topic2)
+    subscriber.start_polling()
 
     with ThreadPoolExecutor(max_workers=num_clients) as executor:
         for client_id in range(num_clients):
             message = generate_random_message()
             executor.submit(send_message, client_id, broker_url, topic, message)
+            #executor.submit(send_message, client_id, broker_url, topic2, message)
             time.sleep(1 / num_clients)
 
-    with ThreadPoolExecutor(max_workers=num_clients) as executor:
-        for client_id in range(num_clients, num_clients*2):
-            message = generate_random_message()
-            executor.submit(send_message, client_id, broker_url, topic2, message)
-            time.sleep(1 / num_clients)
+
 
     print(time.time() - start)
 
